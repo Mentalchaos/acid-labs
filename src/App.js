cb84324c378ab6e3bd2cb2ef83bb2e5674e675c2
mentalchaos <jpierre22391@hotmail.com>
2020-10-17 02:55:34 -0300

    added all initial folder to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Logo from './components/Logo';
+import './css/all.css';
 
 const  App = () => (
   <div className="App">
-    <div>Just testing</div>
+    <Logo />
+    <p>Aqui deberia estar la mejor pagina de la vida, pero no lo esta porque aun no he avanzado en nada XD</p>
+    <p>Pero la configuracion inicial esta lista :D</p>
   </div>
 );
 
---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/logo-acid.png';
+
+const Logo = () => {
+    return (
+        <div className="logo-container">
+            <img alt="acid-labs-logo" className="acid-logo" src={logo} />
+        </div>
+    )
+}
+
+Logo.displayName = 'Logo';
+
+export default Logo;
